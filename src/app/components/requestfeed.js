f15759df111639a4dd32a37704a24aa1f8e4ea81
ahmadsohailMask01 <ahmaddd.sohail759@gmail.com>
2024-04-07 22:44:24 +0500

    third commit

--- src/app/components/requestfeed.js
+++ src/app/components/requestfeed.js
@@ -1,8 +1,18 @@
 import React from "react";
-import styles from '../../../styles/requestfeed.module.css'
+import styles from "../../../styles/requestfeed.module.css";
 
 const RequestFeedback = () => {
-  return <>This is request feedback</>;
+  return (
+    <>
+      <section className={styles.req_section}>
+        <div className={styles.sec_container}>
+          <span>Adhoc Feedback</span>
+          <span>Mid Year Feedback</span>
+          <span>360 Feedback</span>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default RequestFeedback;
